fd1013f00b449e2c5c0e14dc7dfe8d38649d67db
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2024-06-25 14:24:03 +0530

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import cartReducer from './cartReducer'; // Create your cartReducer
-
-const rootReducer = combineReducers({
-  cart: cartReducer,
-  // other reducers if any
-});
-
-export default rootReducer;
--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers'; // Assuming you have a rootReducer
-
-const store = createStore(rootReducer);
-
-export default store;
