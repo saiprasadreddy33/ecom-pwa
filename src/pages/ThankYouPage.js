57f10cf67ec71827a2bf1325a93e992bf5b81225
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2024-06-25 18:23:11 +0530

    ThankYouPage.js axs

--- src/pages/ThankYouPage.js
+++ src/pages/ThankYouPage.js
@@ -1,4 +1,3 @@
-// ThankYouPage.js
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
@@ -10,13 +9,16 @@ const ThankYouPage = ({ location, clearCart, showAlert }) => {
     clearCart();
     showAlert('Thank you for your purchase!');
   }, [clearCart, showAlert]); 
+
   if (!location || !location.state) {
     // Handle case where location or location.state is undefined (optional)
     return (
-      <div className="container mx-auto mt-8">
-        <h2 className="text-2xl font-semibold mb-4">Thank You</h2>
-        <p>Thank you for your purchase!</p>
-        <Link to="/" className="text-blue-500 hover:underline mt-4 block">Continue Shopping</Link>
+      <div className="container mx-auto mt-8 p-6 text-center">
+        <div className="animate-fadeIn">
+          <h2 className="text-4xl font-semibold mb-4">Thank You</h2>
+          <p className="text-lg">Thank you for your purchase!</p>
+          <Link to="/" className="text-blue-500 hover:underline mt-4 block text-lg">Continue Shopping</Link>
+        </div>
       </div>
     );
   }
@@ -24,14 +26,17 @@ const ThankYouPage = ({ location, clearCart, showAlert }) => {
   const { product, quantity } = location.state;
 
   return (
-    <div className="container mx-auto mt-8">
-      <h2 className="text-2xl font-semibold mb-4">Thank You</h2>
-      <p>Thank you for your purchase!</p>
-      <p>You have successfully added {quantity} {product.title}(s) to your cart.</p>
-      <Link to="/" className="text-blue-500 hover:underline mt-4 block">Continue Shopping</Link>
+    <div className="container mx-auto mt-8 p-6 text-center">
+      <div className="animate-fadeIn">
+        <h2 className="text-4xl font-semibold mb-4">Thank You</h2>
+        <p className="text-lg">Thank you for your purchase!</p>
+        <p className="text-lg">You have successfully added {quantity} {product.title}(s) to your cart.</p>
+        <Link to="/" className="text-blue-500 hover:underline mt-4 block text-lg">Continue Shopping</Link>
+      </div>
     </div>
   );
 }
+
 const mapDispatchToProps = {
   clearCart,
   showAlert,
