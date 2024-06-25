e0f4b7d862bd35eab6d382ba9699e98614f96f38
Sai Prasad Reddy <74254400+saiprasadreddy33@users.noreply.github.com>
2024-06-25 18:24:02 +0530

    CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ export const content = [
   './src/**/*.{js,jsx,ts,tsx}',
   './public/index.html',
 ];
+
 export const theme = {
-  extend: {},
+  extend: {
+    keyframes: {
+      fadeIn: {
+        '0%': { opacity: 0 },
+        '100%': { opacity: 1 },
+      },
+    },
+    animation: {
+      fadeIn: 'fadeIn 1s ease-in-out',
+    },
+  },
 };
+
 export const plugins = [];
